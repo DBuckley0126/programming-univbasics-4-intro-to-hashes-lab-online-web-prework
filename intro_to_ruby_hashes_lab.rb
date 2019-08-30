343546a36a7909859f5f4cd48450ae3db0a1c501
Danny Buckley <dannyboy.msn@hotmail.com>
2019-08-30 20:17:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,8 @@ def id_generator
   my_hash3 = {
     id: 3
   }
+  id_generator
+end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
